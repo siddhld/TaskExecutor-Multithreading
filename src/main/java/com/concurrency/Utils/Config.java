c605ca1232bfc5f778d1ec2e4dd88bffd7c21d63
Siddharth Haldhar <90497078+siddhld@users.noreply.github.com>
2024-05-16 17:27:07 +0530

    Update Config.java

--- src/main/java/com/concurrency/Utils/Config.java
+++ src/main/java/com/concurrency/Utils/Config.java
@@ -8,17 +8,22 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @Configuration
 public class Config {
 
+     @Bean()
+	public TaskExecutor threadPoolTaskExecutor() {
+		return new SimpleAsyncTaskExecutor();
+	}
+     
      ////// Using custom configuration
-     @Bean
-     public TaskExecutor taskExecutor() {
-          ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-          executor.setCorePoolSize(4); // Set initial number of threads
-          executor.setMaxPoolSize(10); // Set maximum number of threads
-          executor.setQueueCapacity(25); // Set queue size for waiting tasks
-          executor.setThreadNamePrefix("custom-thread"); // Set thread name prefix
-          executor.initialize(); // Initialize the thread pool
-          return executor;
-     }
+     // @Bean
+     // public TaskExecutor taskExecutor() {
+     //      ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+     //      executor.setCorePoolSize(4); // Set initial number of threads
+     //      executor.setMaxPoolSize(10); // Set maximum number of threads
+     //      executor.setQueueCapacity(25); // Set queue size for waiting tasks
+     //      executor.setThreadNamePrefix("custom-thread"); // Set thread name prefix
+     //      executor.initialize(); // Initialize the thread pool
+     //      return executor;
+     // }
 
      ////// Using default configuration
      // @Bean
